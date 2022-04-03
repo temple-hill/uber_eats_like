a41177449d469c2359c6ae00dfc03d9db5cf4374
teraoka <temple.hill@icloud.com>
2022-04-03 22:39:20 +0900

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+%w[すき家 吉野家 松屋].map do |name|
+  restaurant = Restaurant.new(
+    name: name,
+    fee: 100,
+    time_required: 10,
+  )
+
+  12.times do |i|
+    restaurant.foods.build(
+      name: "牛丼_#{i}",
+      price: 500,
+      description: "牛丼_#{i}の説明文です。"
+    )
+  end
+
+  restaurant.save!
+end
